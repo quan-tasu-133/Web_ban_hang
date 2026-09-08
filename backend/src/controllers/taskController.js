dd19f704db1d69a631667846789a1d0105302bad
quan-tasu-133 <quan133200666@gmail.com>
2026-09-08 17:24:29 +0700

    FirstBackend

---
+++ backend/src/controllers/taskController.js
@@ -0,0 +1,92 @@
+import pool from "../db/database.js";
+
+export const getTasks = async (req, res) => {
+    try {
+        const result = await pool.query("SELECT * FROM tasks");
+
+        res.json(result.rows);
+    } catch (error) {
+        console.error(error);
+
+        res.status(500).json({
+            message: "Database error"
+        });
+    }
+};
+
+export const createTask = async (req, res) => {
+    try {
+        const { title } = req.body;
+
+        const result = await pool.query(
+            "INSERT INTO tasks (title) VALUES ($1) RETURNING *",
+            [title]
+        );
+
+        res.status(201).json(result.rows[0]);
+    } catch (error) {
+        console.error(error);
+
+        res.status(500).json({
+            message: "Database error"
+        });
+    }
+};
+
+export const updateTask = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const { title, completed } = req.body;
+
+        const result = await pool.query(
+            `UPDATE tasks
+             SET title = COALESCE($1, title),
+                 completed = COALESCE($2, completed)
+             WHERE id = $3
+             RETURNING *`,
+            [title, completed, id]
+        );
+
+        if (result.rows.length === 0) {
+            return res.status(404).json({
+                message: "Task not found"
+            });
+        }
+
+        res.json(result.rows[0]);
+    } catch (error) {
+        console.error(error);
+
+        res.status(500).json({
+            message: "Database error"
+        });
+    }
+};
+
+export const deleteTask = async (req, res) => {
+    try {
+        const { id } = req.params;
+
+        const result = await pool.query(
+            "DELETE FROM tasks WHERE id = $1 RETURNING *",
+            [id]
+        );
+
+        if (result.rows.length === 0) {
+            return res.status(404).json({
+                message: "Task not found"
+            });
+        }
+
+        res.json({
+            message: "Task deleted successfully",
+            task: result.rows[0]
+        });
+    } catch (error) {
+        console.error(error);
+
+        res.status(500).json({
+            message: "Database error"
+        });
+    }
+};
--- backend/src/routes/userRoutes.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { getUserById } from "../controllers/userController.js";
-
-const router = express.Router();
-
-router.get("/:id", getUserById);
-
-export default router;
